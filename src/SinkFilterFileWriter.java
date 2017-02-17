05f81756181873384b64e75d1a66e225ab1723e4
bsanchin <battursanchin@gmail.com>
2017-02-17 00:37:42 -0500

    Removed redundant data file. SinkFilterFileWriter now reports correct bytes written.

--- src/SinkFilterFileWriter.java
+++ src/SinkFilterFileWriter.java
@@ -4,65 +4,68 @@ import java.io.PrintWriter;
 
 /**
  * Writes frames of measurement data to a given file.
+ * 
  * @author team-one
  */
 public class SinkFilterFileWriter extends FilterFramework {
-  
+
   private File outFile;
   private static final String NEWLINE = "\n";
-  
+
   public SinkFilterFileWriter(String argFilename) {
-    outFile = new File(argFilename); 
+    outFile = new File(argFilename);
   }
-  
+
   public void run() {
     try {
-      int bytesread = 0;    // Number of bytes read from the input file.
+      int bytesread = 0; // Number of bytes read from the input file.
       int byteswritten = 0; // Number of bytes written to the stream.
-      byte databyte = 0;    // The byte of data read from the file
-  
+      byte databyte = 0; // The byte of data read from the file
+
       // Next we write a message to the terminal to let the world know we are
       // alive...
-  
+
       FrameReader frameReader = new FrameReader();
       PrintWriter buffWriter = new PrintWriter(outFile);
-  
+
       while (true) {
         /*************************************************************
          * Here we read a byte and write a byte
          *************************************************************/
-  
+
         try {
           databyte = ReadFilterInputPort();
           bytesread++;
           frameReader.put(databyte);
-  
+
           if (frameReader.hasFrameAvailable()) {
 
-          Frame f = frameReader.pollFrame();
+            Frame f = frameReader.pollFrame();
             buffWriter.write(f.toString());
             buffWriter.write(NEWLINE);
+            byteswritten += f.toByteArray().length;
           }
-  
+
         } // try
-  
+
         catch (EndOfStreamException e) {
           frameReader.stop();
           while (frameReader.hasFrameAvailable()) {
-          Frame f = frameReader.pollFrame();
-            buffWriter.write(f.toString());  
+            Frame f = frameReader.pollFrame();
+            buffWriter.write(f.toString());
+            byteswritten += f.toByteArray().length;
           }
           ClosePorts();
           System.out.print(
               "\n" + this.getName() + "::Middle Exiting; bytes read: " + bytesread + " bytes written: " + byteswritten);
           break;
-  
+
         } // catch
-  
+
       } // while
       buffWriter.flush();
       buffWriter.close();
-      
+
     } catch (IOException e1) {
       e1.printStackTrace();
     }
